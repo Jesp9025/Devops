4a7d62737cc64b4013c7f7377d3fe0d3c2fde471
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2020-08-29 17:14:43 +0200

    Update configAD.ps1

--- configAD.ps1
+++ configAD.ps1
@@ -1,10 +1,2 @@
-#Install windows feature: 
-Install-WindowsFeature -Name AD-Domain-Services
-#Save password in variable for Safe Mode: 
-$Password = Read-Host -Prompt   ‘Testpassword123’ -AsSecureString
-#Set password of local administrator:
-$username = Read-Host "Type the username:";
-$user = [ADSI]"WinNT://./$username";
-$user.SetPassword("Testpassword123");
-#Add new forest (aka create your domain):
-Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath C:\Windows\NTDS -DomainMode WinThreshold -DomainName burger.local -DomainNetbiosName burger -ForestMode WinThreshold -InstallDns:$true -LogPath C:\Windows\NTDS -NoRebootOnCompletion:$true -SafeModeAdministratorPassword $Password -SysvolPath C:\Windows\SYSVOL -Force:$true
+param ($input1, $input2)
+Write-Host "$input1 $input2"
