673fe97bc2f2a50fbcaad06fa39a821e94061fa8
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2020-08-30 12:25:08 +0200

    Update configAD.ps1

--- configAD.ps1
+++ configAD.ps1
@@ -1,3 +1,8 @@
 echo "Hello Mr. Good day, sir!"
-mkdir $env:SystemDrive\'Hello from Azure Devops'
-Restart-Computer -force
+$folder = "Hello from Azure Devops"
+Try {
+mkdir $env:SystemDrive\"$folder"
+}
+Catch {
+echo "The folder $folder already exists. Skipping..."
+}
