9fa2958ccc92674ad60122f6ef287c704405d40c
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2020-08-30 12:30:35 +0200

    Update configAD.ps1

--- configAD.ps1
+++ configAD.ps1
@@ -1,8 +1,12 @@
-echo "Hello Mr. Good day, sir!"
 $folder = "Hello from Azure Devops"
+
+echo "Hello from Azure.
+Attempting to create folder: $folder..."
+
 Try {
 mkdir $env:SystemDrive\"$folder"
+echo "The folder "$folder" has been created."
 }
 Catch {
-echo "The folder $folder already exists. Skipping..."
+echo "The folder "$folder" already exists. Skipping..."
 }
