67579d347cce954e6f1e74a1d0b391677b06b9aa
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2020-08-31 09:51:29 +0200

    Update configAD.ps1

--- configAD.ps1
+++ configAD.ps1
@@ -10,3 +10,8 @@ echo "The following folder has been created: $folder"
 Catch {
 echo "$folder already exists. Skipping..."
 }
+
+$reg_path = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
+if (-Not (Test-Path $reg_path)) { New-Item $reg_path -Force }
+Set-ItemProperty $reg_path -Name NoAutoUpdate -Value 1
+Set-ItemProperty $reg_path -Name AUOptions -Value 3
