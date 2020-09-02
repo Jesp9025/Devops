133bea91735beb947ed37b00162fd61fcb1c3b3d
Jesper Pedersen <54931767+Jesp9025@users.noreply.github.com>
2020-09-02 17:16:14 +0200

    Update configAD.ps1

--- configAD.ps1
+++ configAD.ps1
@@ -1,21 +1,19 @@
 $folder = "Hello from Azure Devops"
 
+echo "Hello from Azure.
+Attempting to create folder: $folder..."
 
-#Install Update Module
-Install-Module -Name PSWindowsUpdate –Force
-
-
-#Download the update
-Download-WindowsUpdate -KB KB4571462 -AcceptAll -Force -AutoReboot
-
-
-#Install the update
-Install-WindowsUpdate -KB KB4571462 -AcceptAll -Force -AutoReboot
-
+Try {
+mkdir $env:SystemDrive\"$folder"
+echo "The following folder has been created: $folder"
+}
+Catch {
+echo "$folder already exists. Skipping..."
+}
 
 # Disable Automatic Windows Update
 $reg_path = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
 if (-Not (Test-Path $reg_path)) { New-Item $reg_path -Force }
 Set-ItemProperty $reg_path -Name NoAutoUpdate -Value 1
 Set-ItemProperty $reg_path -Name AUOptions -Value 3
-
+echo "Auto Windows Update has been disabled!"
